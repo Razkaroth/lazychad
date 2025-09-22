c438964d30257e629941020a429b41deb9dd43ce
razkaroth <rocker.ikaros@gmail.com>
2025-09-22 16:15:24 -0600

    mid

--- lua/keymaps/zk.lua
+++ lua/keymaps/zk.lua
@@ -3,8 +3,11 @@ local opts = { noremap = true, silent = false }
 
 -- zk configuration
 
--- Create a new note after asking for its title.
-map("n", "<leader>zn", "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>", opts)
+-- Create new notes by type
+map("n", "<leader>znw", "<Cmd>ZkNew { group = 'whisper', title = vim.fn.input('Whisper Title: ') }<CR>", opts)
+map("n", "<leader>znr", "<Cmd>ZkNew { group = 'record', title = vim.fn.input('Record Title: ') }<CR>", opts)
+map("n", "<leader>znc", "<Cmd>ZkNew { group = 'chronicle' }<CR>", opts)
+map("n", "<leader>zne", "<Cmd>ZkNew { group = 'etching', title = vim.fn.input('Etching Title: ') }<CR>", opts)
 
 -- Open notes.
 map("n", "<leader>zo", "<Cmd>ZkNotes { sort = { 'modified' } }<CR>", opts)
@@ -29,3 +32,63 @@ map("n", "<leader>zf", "<Cmd>ZkNotes { sort = { 'modified' }, match = { vim.fn.i
 
 -- Search for the notes matching the current visual selection.
 map("v", "<leader>zf", ":'<,'>ZkMatch<CR>", opts)
+
+-- Smart link navigation: follow definition or create new note
+local function smart_link_goto()
+  -- Try LSP definition first
+  local params = vim.lsp.util.make_position_params(0, "utf-8")
+  local clients = vim.lsp.get_clients({ bufnr = 0 })
+  
+  if #clients == 0 then
+    vim.notify("No LSP client attached", vim.log.levels.WARN)
+    return
+  end
+
+  -- Request definition from LSP
+  vim.lsp.buf_request(0, "textDocument/definition", params, function(err, result, ctx, config)
+    if err or not result or vim.tbl_isempty(result) then
+      -- No definition found, create new note
+      
+      -- Get the word under cursor (potential link text)
+      local word = vim.fn.expand("<cword>")
+      local line = vim.api.nvim_get_current_line()
+      local col = vim.fn.col(".")
+      
+      -- Try to extract wikilink text [[text]]
+      local link_text = word
+      local start_pos = line:find("%[%[[^%]]*", col - #word)
+      if start_pos then
+        local end_pos = line:find("%]%]", start_pos)
+        if end_pos then
+          link_text = line:sub(start_pos + 2, end_pos - 1)
+        end
+      end
+      
+      -- Ask user which type of note to create
+      vim.ui.select(
+        { "whisper", "record", "chronicle", "etching" },
+        { prompt = "Select note type:" },
+        function(choice)
+          if choice then
+            local handle, path
+            if choice == "chronicle" then
+              -- Chronicle doesn't need title input
+              handle = io.popen('zk nvim-chronicle')
+            else
+              -- Other types use the link text as title
+              handle = io.popen(string.format('zk nvim-%s "%s"', choice, link_text))
+            end
+            path = handle:read("*a"):gsub("%s+", "")
+            handle:close()
+            vim.cmd('edit ' .. path)
+          end
+        end
+      )
+    else
+      -- Definition found, go to it
+      vim.lsp.util.jump_to_location(result[1], "utf-8")
+    end
+  end)
+end
+
+map("n", "<leader>zgd", smart_link_goto, opts)
